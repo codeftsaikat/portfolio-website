4ebfbcd4d7b986bd96b897da21f0b91ae090bc42
codeftsaikat <soikatdey18@gmail.com>
2025-01-04 05:58:18 +0600

    Implement Hero section using aceternity ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="text-4xl bg-black-100">Hello, Portfolio</h1>
+        <Hero />
       </div>
     </main>
   );
